0bc06c2d2d850bc73763aed0e41b6c09eec25150
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-09 13:37:18 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# install stubby
+sudo apt-get -y install stubby
+
+# clean stubby config
+mkdir -p /etc/stubby \
+    && rm -f /etc/stubby/stubby.yml
+
+# install cloudflared
+# partially done in Dockerfile due to platform difference
+useradd -s /usr/sbin/nologin -r -M cloudflared \
+    && chown cloudflared:cloudflared /usr/local/bin/cloudflared
+    
+# clean cloudflared config
+RUN mkdir -p /etc/cloudflared \
+    && rm -f /etc/cloudflared/config.yml
